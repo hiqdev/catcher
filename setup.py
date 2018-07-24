b5b22d50b2ce49cfca59d2b6a4c7aab9bfb180bf
Andrii Vasyliev <sol@hiqdev.com>
2018-07-24 11:00:03 -0400

    adding pypi packaging

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'catcher',
+    packages = ['catcher'],
+    version = '0.0.1',
+    description = 'EPP drop/catch system',
+    author = 'Andrii Vasyliev',
+    author_email = 'sol@hiqdev.com',
+    url = 'https://github.com/hiqdev/catcher',
+    download_url = 'https://github.com/hiqdev/catcher/tarball/0.0.1',
+    keywords = ['epp', 'drop/catch'],
+    classifiers = [],
+)
