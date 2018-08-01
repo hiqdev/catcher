91da4cc3dda8c9b05c26363b91f3ef77e799e0f8
Andrii Vasyliev <sol@hiqdev.com>
2018-08-01 15:30:27 +0000

    Started `Catcher` class

--- catcher/Catcher.py
+++ catcher/Catcher.py
@@ -1,4 +1,71 @@
+#!/usr/bin/env python
+
+import time
+from datetime import timedelta
+from datetime import datetime
+
+from pprint import pprint
+
+from catcher.Random import Random
+from heppy.Config import Config
+from heppy.Daemon import Daemon
 
 class Catcher:
-    def test(self):
-        print "CATCHER"
+    def __init__(self, config):
+        self.config = config
+        self.period = timedelta(**config['mainRefreshPeriod'])
+        self.refreshed = datetime.now()
+        self.domains = {}
+        self.reload_domains()
+        self.check_domains()
+
+    def run(self):
+        #self.daemon = Daemon(config)
+        #self.daemon.start()
+        print "STARTED"
+        self.loop()
+
+    def loop(self):
+        while True:
+            time.sleep(0.5)
+            print '  ' + self.random.random_key()
+            self.refresh()
+
+    def refresh(self):
+        if datetime.now() < self.refreshed + self.period:
+            return
+
+        print "- refresh"
+        self.refreshed = datetime.now()
+        if self.refreshed > self.domainsLastReloaded + self.domainsReloadPeriod:
+            self.reload_domains()
+        if self.refreshed > self.domainsLastChecked + self.domainsCheckPeriod:
+            self.check_domains()
+
+    def reload_domains(self):
+        alldoms = Config(self.config['domainsFilePath'])
+
+        for domain in alldoms:
+            since = datetime.strptime(alldoms[domain]['since'], '%Y-%m-%d %H:%M:%S')
+            if since > datetime.now() or 'taken-by' in alldoms[domain]:
+                alldoms[domain]['disabled'] = True
+            else:
+                if domain in self.domains:
+                    self.domains[domain].update(alldoms[domain])
+                else:
+                    self.domains[domain] = alldoms[domain]
+
+        for domain in self.domains.keys():
+            if not domain in alldoms or 'disabled' in alldoms[domain]:
+                del self.domains[domain]
+
+        self.random = Random(self.domains)
+        pprint(self.domains)
+        print "! DOMAINS RELOADED"
+        self.domainsLastReloaded = datetime.now()
+        self.domainsReloadPeriod = timedelta(**self.config['domainsReloadPeriod'])
+
+    def check_domains(self):
+        self.domainsLastChecked = datetime.now()
+        self.domainsCheckPeriod = timedelta(**self.config['domainsCheckPeriod'])
+        print "! DOMAINS CHECKED"
